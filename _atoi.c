38dbc311aa3c6122045662ea3a2c234152bcac66
USRGS0384 <philipajuong@gmail.com>
2023-11-15 17:31:39 -0800

    Solo Task

---
+++ _atoi.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * interactive - display true if shell is interactive mode
+ * @info: address struct
+ * Return: interactive mode is 1 else 0
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - verify if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: true if 1 else 0 on false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - detect if alphabetic character is available
+ *@c: The character to be recorded
+ *Return: if c is alphabetic then 1 else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - converts an an integer to string
+ *@s: string to be converted
+ *Return: if no numbers in string then 0 else converted number
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
